7f84ba2b5a4ef6366cbcc6cb092f93517fc7e980
alto-I <blmaldiy@gmail.com>
2022-07-17 10:35:56 +0900

    8章終わり

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,20 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  it "generates associated data from a factory" do
-    note = FactoryBot.create(:note)
-  end
-
-  before do
-    @user = FactoryBot.create(:user)
-
-    @project = @user.projects.create(
-      name: "Test Project"
-    )
-  end
+    let(:user) { FactoryBot.create(:user) }
+    let(:project) { FactoryBot.create(:project, owner: user) }
 
   it "is invalid with a user, project, and message" do
-    note = FactoryBot.build(:note)
+    note = Note.new(
+      message: "This is a sample note.",
+      user: user,
+      project: project
+    )
     expect(note).to be_valid
   end
 
@@ -25,22 +20,41 @@ RSpec.describe Note, type: :model do
   end
 
   describe "search message for a term" do
-    before do
-      @note1 = FactoryBot.create(:note, message: "This is the first note.")
-      @note2 = FactoryBot.create(:note, message: "This is the second note.")
-      @note3 = FactoryBot.create(:note, message: "First, This is the third note.")
-    end
+    let!(:note1) {
+      FactoryBot.create(:note,
+        project: project,
+        user: user,
+        message: "This is the first note."
+      )
+    }
+
+    let!(:note2) {
+      FactoryBot.create(:note,
+        project: project,
+        user: user,
+        message: "This is the second note."
+      )
+    }
+
+    let!(:note3) {
+      FactoryBot.create(:note,
+        project: project,
+        user: user,
+        message: "First, preheat the oven."
+      )
+    }
 
     context "when a match is found" do
       it "return notes that match the search term" do
-        expect(Note.search("first")).to include(@note1, @note3)
-        expect(Note.search("first")).to_not include(@note2)
+        expect(Note.search("first")).to include(note1, note3)
+        expect(Note.search("first")).to_not include(note2)
       end
     end
 
     context "when no match is found" do
       it "returns an empty collection when no results are found" do
         expect(Note.search("message")).to be_empty
+        expect(Note.count).to eq 3
       end
     end
   end
